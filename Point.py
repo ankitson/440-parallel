9f74e34eae33fd7910ec27ccae6b5da05db1ea4b
Ankit Soni <ankitsoni9@gmail.com>
2013-12-06 17:59:47 -0800

    more changes

--- Point.py
+++ Point.py
@@ -2,7 +2,6 @@ import math
 import sys
 
 class Point:
-
     x = 0
     y = 0
 
@@ -14,42 +13,46 @@ class Point:
         return cmp(self.x, other.x) and cmp(self.y, other.y)
 
     def __hash__(self):
-	return hash((self.x, self.y))
+        return hash((self.x, self.y))
 
     def __str__(self):
-        return '(' + str(self.x) + ', ' + str(self.y) + ')'
-    
+        return '(' + str(self.x) + ',' + str(self.y) + ')'
+
     def distance(self, other):
-        dx = math.fabs(self.x - other.x)
-        dy = math.fabs(self.y - other.y)
-        return math.sqrt(dx * dx + dy * dy)
+        x1 = float(self.x)
+        x2 = float(other.x)
+        y1 = float(self.y)
+        y2 = float(other.y)
 
+        xdist = math.pow(math.abs(x1-x2),2)
+        ydist = math.pow(math.abs(y1-y2),2)
+        return math.sqrt(xdist + ydist)
 
-    # todo
-    def find_closest_point(self, points):
-      
+    #Return the point out of points closest to this one
+    def find_closest_point(self,points):
         if (len(points) == 0):
             return None
-        min_dist = sys.maxint
-        cur_centroid = 0
-        for i in xrange(len(points)):
-            icentroid_dist = self.distance(points[i])
-            if (icentroid_dist <= min_dist):
-                min_dist = icentroid_dist
-                cur_centroid = i
-        return cur_centroid
+        pts_idxs = enumerate(points)
+        pts_indxs = [(idx,self.distance(pt)) for (idx,pt) in pts_idxs]
+
+        mindist = sys.maxint
+        minptidx = 0
+        for (idx,dist) in pts_indxs:
+            if dist < mindist:
+                mindist = dist
+                minptidx = idx
+
+        return points[minptidx]
 
     @staticmethod
     def getAverage(points):
-	    
         if (len(points) == 0):
             return None
-        sum_x = 0
-        sum_y = 0
+
+        xsum = 0.0
+        ysum = 0.0
         for point in points:
-            sum_x += point.x
-            sum_y += point.y
-        return Point(float(sum_x)/len(points), float(sum_y)/len(points))
+            xsum += point.x
+            ysum += point.y
 
-
-
+        return Point(float(xsum)/float(len(points)), float(ysum)/float(len(points)))
